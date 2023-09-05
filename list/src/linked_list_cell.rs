a5b215fe7e33c8264fe4eb1551159611091f4e09
Tomislav Pesut <tomca32@gmail.com>
2023-09-05 14:17:14 +0200

    Implemented push_back

--- list/src/linked_list_cell.rs
+++ list/src/linked_list_cell.rs
@@ -1,4 +1,4 @@
-use std::{borrow::BorrowMut, cell::RefCell, rc::Rc};
+use std::cell::RefCell;
 
 #[derive(Default)]
 pub struct LinkedListCell<T> {
@@ -23,19 +23,9 @@ impl<T> LinkedListCell<T> {
     }
 
     pub fn push_back(&mut self, value: T) {
-        if self.is_empty() {
-            self.head = Node::new_boxed(value);
-            return;
-        }
-
-        let mut current = self.head.as_ref().unwrap();
-        loop {
-            if current.borrow().next.is_none() {
-                current.borrow_mut().next = Node::new_boxed(value);
-                break;
-            }
-
-            //current = current.borrow().next;
+        match self.head {
+            None => self.head = Node::new_boxed(value),
+            Some(ref mut n) => n.borrow_mut().push_back(value),
         }
     }
 }
@@ -47,5 +37,64 @@ impl<T> Node<T> {
 
     pub fn new_boxed(value: T) -> Option<RefCell<Box<Node<T>>>> {
         Some(RefCell::new(Box::new(Node::new(value))))
+    }
+
+    pub fn push_back(&mut self, value: T) {
+        match &mut self.next {
+            None => self.next = Node::new_boxed(value),
+            Some(n) => {
+                n.borrow_mut().push_back(value);
+            }
+        }
+    }
+
+    pub fn pop_back(&mut self) -> T {
+        match &mut self.next {
+            None => self.next = Node::new_boxed(value),
+            Some(n) => {
+                n.borrow_mut().pop_back(value);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_linked_list_cell() {
+        use super::LinkedListCell;
+
+        let mut list = LinkedListCell::new(1);
+        assert_eq!(list.is_empty(), false);
+
+        list.push_back(2);
+        list.push_back(3);
+        list.push_back(4);
+        list.push_back(5);
+
+        assert_eq!(list.is_empty(), false);
+    }
+
+    #[test]
+    fn test_linked_list_cell_empty() {
+        use super::LinkedListCell;
+
+        let list: LinkedListCell<i32> = LinkedListCell::default();
+        assert_eq!(list.is_empty(), true);
+    }
+
+    #[test]
+    fn test_linked_list_cell_push_back() {
+        use super::LinkedListCell;
+
+        let mut list = LinkedListCell::new(1);
+        assert_eq!(list.is_empty(), false);
+
+        list.push_back(2);
+        list.push_back(3);
+        list.push_back(4);
+        list.push_back(5);
+
+        assert_eq!(list.is_empty(), false);
     }
 }
